006990f9efc7353d9801ab1016b768aaf6d079f4
Shrivara K S <shrivara.ks@icicletech.com>
2015-07-11 12:35:23 +0530

    [Event] Add callbacks for drag and drop.

--- addon/components/full-calendar.js
+++ addon/components/full-calendar.js
@@ -22,6 +22,12 @@ export default Ember.Component.extend({
 
   // Event Dragging & Resizing
   editable				      : false,
+  eventStartEditable    : false,
+  eventDurationEditable : false,
+  dragRevertDuration    : 500,
+  dragOpacity           : 0.75,
+  dragScroll            : true,
+  dragOverlap           : true,
 
   _initializeCalendar: function() {
   	var _this = this;
@@ -41,8 +47,38 @@ export default Ember.Component.extend({
       	_this.sendAction('eventClick', calEvent, jsEvent, view);
       },
 
+      eventDragStart: function(event, jsEvent, ui, view) {
+        _this.sendAction('eventDragStart', event, jsEvent, ui, view);
+      },
+
+      eventDragStop: function(event, jsEvent, ui, view) {
+        _this.sendAction('eventDragStart', event, jsEvent, ui, view);
+      },
+
+      eventDrop: function(event, delta, revertFunc, jsEvent, ui, view) {
+        _this.sendAction('eventDrop', event, delta, revertFunc, jsEvent, ui, view);
+      },
+
+      eventResize: function(function(event, delta, revertFunc, jsEvent, ui, view) {
+        _this.sendAction('eventResize', event, delta, revertFunc, jsEvent, ui, view);
+      },
+
+      eventResizeStart: function(event, jsEvent, ui, view) {
+        _this.sendAction('eventResizeStart', event, jsEvent, ui, view);
+      },
+
+      eventResizeStop: function(event, jsEvent, ui, view) {
+        _this.sendAction('eventResizeStop', event, jsEvent, ui, view);
+      },
+
       // Dragging & Resizing
     	editable: _this.get('editable'),
+      eventStartEditable: _this.get('eventStartEditable'),
+      eventDurationEditable: _this.get('eventDurationEditable'),
+      dragRevertDuration: _this.get('dragRevertDuration'),
+      dragOpacity: _this.get('dragOpacity'),
+      dragScroll: _this.get('dragScroll'),
+
       eventDrop: function(event, delta, revertFunc, jsEvent, ui, view) {
       	_this.sendAction('eventDrop', event, delta, revertFunc, jsEvent, ui, view);
       }
