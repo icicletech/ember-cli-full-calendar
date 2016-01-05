ad2aa265d62d32a098f5061d0b878b36bebd1662
Miguel Andrade <jmandrade0@gmail.com>
2016-01-05 14:12:04 +0000

    es6 goodness and refactoring

--- addon/components/full-calendar.js
+++ addon/components/full-calendar.js
@@ -1,166 +1,167 @@
 import Ember from 'ember';
+const { observer, Component } = Ember;
 
-export default Ember.Component.extend({
+export default Component.extend({
   tagName: 'div',
   classNames: ['full-calendar'],
 
   // Event Data
-  events                : null,
+  events: null,
 
   // General Display
-  headerLeft            : { left: 'title', center: '', right:  'today prev,next' },
-  theme                 : false,
-  firstDay              : 0,
-  isRTL                 : false,
-  weekends              : true,
-  hiddenDays            : [],
-  fixedWeekCount        : true,
-  weekNumbers           : false,
-  height                : 'auto',
-  businessHours         : false,
+  headerLeft: { left: 'title', center: '', right:  'today prev,next' },
+  theme: false,
+  firstDay: 0,
+  isRTL: false,
+  weekends: true,
+  hiddenDays: [],
+  fixedWeekCount: true,
+  weekNumbers: false,
+  height: 'auto',
+  businessHours: false,
 
   // Text/Time Customization
-  lang                  : 'en',
+  lang: 'en',
 
   // Views
-  defaultView           : 'month',
+  defaultView: 'month',
 
   // Agenda Options
-  minTime               : '00:00:00',
-  maxTime               : '24:00:00',
-  scrollTime            : '06:00:00',
-  slotEventOverlap      : true,
+  minTime: '00:00:00',
+  maxTime: '24:00:00',
+  scrollTime: '06:00:00',
+  slotEventOverlap: true,
 
   // Current Date
-  defaultDate           : null,
+  defaultDate: null,
 
   // Event Dragging & Resizing
-  editable              : false,
-  eventStartEditable    : false,
-  eventDurationEditable : false,
-  dragRevertDuration    : 500,
-  dragOpacity           : 0.75,
-  dragScroll            : true,
-  dragOverlap           : true,
+  editable: false,
+  eventStartEditable: false,
+  eventDurationEditable: false,
+  dragRevertDuration: 500,
+  dragOpacity: 0.75,
+  dragScroll: true,
+  dragOverlap: true,
 
   // Selecting
-  selectable            : false,
+  selectable: false,
 
-  updateEvents: function() {
-    var fullCalendarComponent = Ember.$(".full-calendar");
-    fullCalendarComponent.fullCalendar('removeEvents');
-    fullCalendarComponent.fullCalendar('addEventSource', this.get('events'));
-    fullCalendarComponent.fullCalendar('rerenderEvents' );
-  }.observes('events'),
+  updateEvents: observer('events', function() {
+    var fullCalendarElement = this.$();
+    fullCalendarElement.fullCalendar('removeEvents');
+    fullCalendarElement.fullCalendar('addEventSource', this.get('events'));
+    fullCalendarElement.fullCalendar('rerenderEvents' );
+  }),
 
-  _initializeCalendar: function() {
-    var _this = this;
-    return Ember.$(".full-calendar").fullCalendar({
+  didInsertElement() {
+    this._super(...arguments);
+    this.$().fullCalendar({
 
       // Event Data
-      events: _this.get('events'),
-      eventSources: _this.get('eventSources'),
-      timeFormat: _this.get('timeFormat'),
+      events: this.get('events'),
+      eventSources: this.get('eventSources'),
+      timeFormat: this.get('timeFormat'),
 
       // General Display
-      header: _this.get('header'),
-      customButtons: _this.get('customButtons'),
-      theme: _this.get('theme'),
-      firstDay: _this.get('firstDay'),
-      isRTL: _this.get('isRTL'),
-      weekends: _this.get('weekends'),
-      hiddenDays: _this.get('hiddenDays'),
-      fixedWeekCount: _this.get('fixedWeekCount'),
-      weekNumbers: _this.get('weekNumbers'),
-      height: _this.get('height'),
-      defaultView: _this.get('defaultView'),
-      businessHours: _this.get('businessHours'),
-      titleFormat: _this.get('titleFormat'),
+      header: this.get('header'),
+      customButtons: this.get('customButtons'),
+      theme: this.get('theme'),
+      firstDay: this.get('firstDay'),
+      isRTL: this.get('isRTL'),
+      weekends: this.get('weekends'),
+      hiddenDays: this.get('hiddenDays'),
+      fixedWeekCount: this.get('fixedWeekCount'),
+      weekNumbers: this.get('weekNumbers'),
+      height: this.get('height'),
+      defaultView: this.get('defaultView'),
+      businessHours: this.get('businessHours'),
+      titleFormat: this.get('titleFormat'),
 
-      viewRender: function(view, element) {
-        _this.sendAction('viewRender', view, element);
+      viewRender(view, element) => {
+        this.sendAction('viewRender', view, element);
       },
 
       // Timezone
-      timezone: _this.get('timezone'),
+      timezone: this.get('timezone'),
 
       // Agenda Option
-      minTime: _this.get('minTime'),
-      maxTime: _this.get('maxTime'),
-      scrollTime: _this.get('scrollTime'),
-      slotEventOverlap: _this.get('slotEventOverlap'),
-      slotLabelFormat: _this.get('slotLabelFormat'),
+      minTime: this.get('minTime'),
+      maxTime: this.get('maxTime'),
+      scrollTime: this.get('scrollTime'),
+      slotEventOverlap: this.get('slotEventOverlap'),
+      slotLabelFormat: this.get('slotLabelFormat'),
 
       // Current Date
-      defaultDate: _this.get('defaultDate'),
+      defaultDate: this.get('defaultDate'),
 
       // Text/Time Customization
-      lang: _this.get('lang'),
+      lang: this.get('lang'),
 
       // Clicking & Hovering
-      dayClick: function(date, jsEvent, view) {
-        _this.sendAction('dayClick', date, jsEvent, view);
+      dayClick(date, jsEvent, view) => {
+        this.sendAction('dayClick', date, jsEvent, view);
       },
 
-      eventClick: function(calEvent, jsEvent, view) {
-        _this.sendAction('eventClick', calEvent, jsEvent, view);
+      eventClick(calEvent, jsEvent, view) => {
+        this.sendAction('eventClick', calEvent, jsEvent, view);
       },
 
-      eventDragStart: function(event, jsEvent, ui, view) {
-        _this.sendAction('eventDragStart', event, jsEvent, ui, view);
+      eventDragStart(event, jsEvent, ui, view) => {
+        this.sendAction('eventDragStart', event, jsEvent, ui, view);
       },
 
-      eventDragStop: function(event, jsEvent, ui, view) {
-        _this.sendAction('eventDragStop', event, jsEvent, ui, view);
+      eventDragStop(event, jsEvent, ui, view) => {
+        this.sendAction('eventDragStop', event, jsEvent, ui, view);
       },
 
-      eventDrop: function(event, delta, revertFunc, jsEvent, ui, view) {
-        _this.sendAction('eventDrop', event, delta, revertFunc, jsEvent, ui, view);
+      eventDrop(event, delta, revertFunc, jsEvent, ui, view) => {
+        this.sendAction('eventDrop', event, delta, revertFunc, jsEvent, ui, view);
       },
 
-      eventResize: function(event, delta, revertFunc, jsEvent, ui, view) {
-        _this.sendAction('eventResize', event, delta, revertFunc, jsEvent, ui, view);
+      eventResize(event, delta, revertFunc, jsEvent, ui, view) => {
+        this.sendAction('eventResize', event, delta, revertFunc, jsEvent, ui, view);
       },
 
-      eventResizeStart: function(event, jsEvent, ui, view) {
-        _this.sendAction('eventResizeStart', event, jsEvent, ui, view);
+      eventResizeStart(event, jsEvent, ui, view) => {
+        this.sendAction('eventResizeStart', event, jsEvent, ui, view);
       },
 
-      eventResizeStop: function(event, jsEvent, ui, view) {
-        _this.sendAction('eventResizeStop', event, jsEvent, ui, view);
+      eventResizeStop(event, jsEvent, ui, view) => {
+        this.sendAction('eventResizeStop', event, jsEvent, ui, view);
       },
 
-      select: function(start, end, jsEvent, view) {
-        _this.sendAction('select', start, end, jsEvent, view);
+      select(start, end, jsEvent, view) => {
+        this.sendAction('select', start, end, jsEvent, view);
       },
 
       // Event Rendering
-      eventRender: function(event, element, view) {
-        _this.sendAction('eventRender', event, element, view);
+      eventRender(event, element, view) => {
+        this.sendAction('eventRender', event, element, view);
       },
 
-      eventAfterRender: function( event, element, view ) {
-        _this.sendAction('eventAfterRender', event, element, view);
+      eventAfterRender(event, element, view ) => {
+        this.sendAction('eventAfterRender', event, element, view);
       },
 
-      eventAfterAllRender: function (view) {
-        _this.sendAction('eventAfterAllRender', view);
+      eventAfterAllRender(view) => {
+        this.sendAction('eventAfterAllRender', view);
       },
 
-      eventDestroy: function (event, element, view) {
-        _this.sendAction('eventDestroy', event, element, view);
+      eventDestroy(event, element, view) => {
+        this.sendAction('eventDestroy', event, element, view);
       },
 
       // Dragging & Resizing
-      editable: _this.get('editable'),
-      eventStartEditable: _this.get('eventStartEditable'),
-      eventDurationEditable: _this.get('eventDurationEditable'),
-      dragRevertDuration: _this.get('dragRevertDuration'),
-      dragOpacity: _this.get('dragOpacity'),
-      dragScroll: _this.get('dragScroll'),
+      editable: this.get('editable'),
+      eventStartEditable: this.get('eventStartEditable'),
+      eventDurationEditable: this.get('eventDurationEditable'),
+      dragRevertDuration: this.get('dragRevertDuration'),
+      dragOpacity: this.get('dragOpacity'),
+      dragScroll: this.get('dragScroll'),
 
       // Selecting
-      selectable: _this.get('selectable')
+      selectable: this.get('selectable')
     });
-  }.on('didInsertElement')
+  }
 });
