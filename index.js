ca6a7fa2b3afd6574019a9dbf4f3fc76f525754c
Miguel Andrade <jmandrade0@gmail.com>
2016-01-05 12:24:55 +0000

    do not use minified files

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = {
 
   included: function(app) {
     this._super.included(app);
-    
-    app.import(app.bowerDirectory + '/fullcalendar/dist/fullcalendar.min.js');
-    app.import(app.bowerDirectory + '/fullcalendar/dist/fullcalendar.min.css');
+
+    app.import(app.bowerDirectory + '/fullcalendar/dist/fullcalendar.js');
+    app.import(app.bowerDirectory + '/fullcalendar/dist/fullcalendar.css');
   }
 };
